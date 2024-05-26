a58df89e2ac8e2e149cb36b873cc8054796f66a1
zakiroc <164227048+zakiroc@users.noreply.github.com>
2024-05-26 15:25:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let string= prompt('string typedagi malumot kiriting:')
+let number= prompt('number typedagi malumot kiriting:')
+let boolean= prompt('boolean typedagi malumot kiriting:')
+confirm ('hamma kiritgan malumotlariz togrimi ?')
+
+alert (`
+siz kiritgan ${string} sozi ${typeof(string)} turida yozilgan
+siz kiritgan ${number} sozi ${typeof(number)} turida yozilgan
+siz kiritgan ${boolean} sozi ${typeof(boolean == boolean)} turida yozilgan
+`)
+
